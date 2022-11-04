0285e7c288e69933b1737bdb83fdccae17b1bbf7
dowon <ks2515@naver.com>
2022-11-04 11:49:04 +0900

    feat: train.sh

---
+++ train.sh
@@ -0,0 +1,8 @@
+python main.py \
+    --optimizer adamw_bnb \
+    --batch-size 32 \
+    --num-workers 12 \
+    --max-epochs 100 \
+    --steps-per-epoch 10000 \
+    --seed 221104 \
+    --fast-dev-run
