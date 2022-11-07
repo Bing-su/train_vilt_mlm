861a048788334ecfb8d9ea5e0489b116f0c782e9
dowon <ks2515@naver.com>
2022-11-07 16:16:53 +0900

    fix: checkpoints filename 2

--- main.py
+++ main.py
@@ -79,7 +79,6 @@ def train(
     checkpoints = ModelCheckpoint(
         monitor="train/loss_epoch",
         save_last=True,
-        filename="{epoch:02d}-{train_loss:.2f}",
         save_top_k=3,
         mode="min",
     )
